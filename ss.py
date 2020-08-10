500a0b8a26e513029775dacbba5d30ccb578f0b8
ziyonglixian <68988540+ziyonglixian@users.noreply.github.com>
2020-08-10 16:06:45 +0800

    Update ss.py

--- ss.py
+++ ss.py
@@ -42,5 +42,5 @@ headers3 = {
     "cookie":
     "yj0M_ae15_saltkey=n0ApaVFI; yj0M_ae15_lastvisit=1596979844; yj0M_ae15_ulastactivity=1597046078%7C0; yj0M_ae15_auth=659bo6fiKjfjYErypokCyV6mSSVv3USE123GmzmMVlD0a1sCkusWOJQc%2Bm0mjLS%2B0pWLQFdC4RAmFplTGu224ozMq9E; yj0M_ae15_lastcheckfeed=934165%7C1597046078; yj0M_ae15_lip=13.78.14.245%2C1597046078; yj0M_ae15_nofavfid=1; yj0M_ae15_noticeTitle=1; yj0M_ae15_sendmail=1; yj0M_ae15_lastact=1597046430%09misc.php%09patch",
 }
-url3 = "https://www.soushu.info/home.php?mod=task&do=draw&id=2"
+url3 = "https://bbs.kansoushu.com/home.php?mod=task&do=draw&id=2"
 res3 = requests.post(url=url3, headers=headers3).text
