66f3e9e64e63397c0c34f9aabf8277f5ef157823
ziyonglixian <68988540+ziyonglixian@users.noreply.github.com>
2020-08-10 16:03:15 +0800

    Update ss.py

--- ss.py
+++ ss.py
@@ -6,9 +6,9 @@ headers1 = {
     "user-agent":
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36",
     "cookie":
-    "yj0M_5ed6_saltkey=vBhezKzY; yj0M_5ed6_lastvisit=1595763031; yj0M_5ed6_auth=8975ntPpE7hIbg74pSyFJ8b6z2N97TQy85SViB2mMiiw7AGUCyL2MJ8uvOzdJZVEXb8giv1%2B8WHHjjsvLIbymi7pnbI; yj0M_5ed6_lastcheckfeed=934165%7C1595766635; yj0M_5ed6_nofavfid=1; yj0M_5ed6_smile=1D1; yj0M_5ed6_home_diymode=1; yj0M_5ed6_ulastactivity=1596715436%7C0; yj0M_5ed6_sendmail=1; yj0M_5ed6_lastact=1596715436%09home.php%09spacecp; yj0M_5ed6_checkpm=1",
+    "yj0M_ae15_saltkey=n0ApaVFI; yj0M_ae15_lastvisit=1596979844; yj0M_ae15_ulastactivity=1597046078%7C0; yj0M_ae15_auth=659bo6fiKjfjYErypokCyV6mSSVv3USE123GmzmMVlD0a1sCkusWOJQc%2Bm0mjLS%2B0pWLQFdC4RAmFplTGu224ozMq9E; yj0M_ae15_lastcheckfeed=934165%7C1597046078; yj0M_ae15_lip=13.78.14.245%2C1597046078; yj0M_ae15_nofavfid=1; yj0M_ae15_noticeTitle=1; yj0M_ae15_sendmail=1; yj0M_ae15_lastact=1597046430%09misc.php%09patch",
 }
-url1 = "https://www.soushu.info/home.php?mod=task&do=apply&id=2"
+url1 = "https://bbs.kansoushu.com/home.php?mod=task&do=apply&id=2"
 res1 = requests.post(url=url1, headers=headers1).text
 
 
@@ -18,7 +18,7 @@ def q():
     x = random.randint(0, 255)
     y = random.randint(0, 255)
     ip = str(m) + "." + str(n) + "." + str(x) + "." + str(y)
-    url = 'https://www.soushu.info/forum.php?fromuid=941145'
+    url = 'https://bbs.kansoushu.com/forum.php?fromuid=934165'
     headers = {
         "user-agent":
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36",
@@ -40,7 +40,7 @@ headers3 = {
     "user-agent":
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36",
     "cookie":
-    "yj0M_5ed6_saltkey=vBhezKzY; yj0M_5ed6_lastvisit=1595763031; yj0M_5ed6_auth=8975ntPpE7hIbg74pSyFJ8b6z2N97TQy85SViB2mMiiw7AGUCyL2MJ8uvOzdJZVEXb8giv1%2B8WHHjjsvLIbymi7pnbI; yj0M_5ed6_lastcheckfeed=934165%7C1595766635; yj0M_5ed6_nofavfid=1; yj0M_5ed6_smile=1D1; yj0M_5ed6_home_diymode=1; yj0M_5ed6_ulastactivity=1596715436%7C0; yj0M_5ed6_sendmail=1; yj0M_5ed6_lastact=1596715436%09home.php%09spacecp; yj0M_5ed6_checkpm=1",
+    "yj0M_ae15_saltkey=n0ApaVFI; yj0M_ae15_lastvisit=1596979844; yj0M_ae15_ulastactivity=1597046078%7C0; yj0M_ae15_auth=659bo6fiKjfjYErypokCyV6mSSVv3USE123GmzmMVlD0a1sCkusWOJQc%2Bm0mjLS%2B0pWLQFdC4RAmFplTGu224ozMq9E; yj0M_ae15_lastcheckfeed=934165%7C1597046078; yj0M_ae15_lip=13.78.14.245%2C1597046078; yj0M_ae15_nofavfid=1; yj0M_ae15_noticeTitle=1; yj0M_ae15_sendmail=1; yj0M_ae15_lastact=1597046430%09misc.php%09patch",
 }
 url3 = "https://www.soushu.info/home.php?mod=task&do=draw&id=2"
 res3 = requests.post(url=url3, headers=headers3).text
