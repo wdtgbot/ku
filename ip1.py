1dee5fbc1ac6423144396233b6834820027ac7cb
ziyonglixian <68988540+ziyonglixian@users.noreply.github.com>
2020-08-10 16:00:12 +0800

    Update ip1.py

--- ip1.py
+++ ip1.py
@@ -9,7 +9,7 @@ def q():
     x = random.randint(0, 255)
     y = random.randint(0, 255)
     ip = str(m) + "." + str(n) + "." + str(x) + "." + str(y)
-    url = 'https://www.soushu.info/forum.php?fromuid=934165'
+    url = 'https://bbs.kansoushu.com/forum.php?fromuid=934165'
     headers = {
         "user-agent":
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36",
