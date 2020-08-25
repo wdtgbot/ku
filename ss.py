7a6677710fa8be681af306b3fb053daea183e755
ziyonglixian <68988540+ziyonglixian@users.noreply.github.com>
2020-08-25 14:28:41 +0800

    Update ss.py

--- ss.py
+++ ss.py
@@ -2,11 +2,11 @@ import random
 import threading
 import requests
 
+cookie = os.environ["COOKIESS"]
 headers1 = {
     "user-agent":
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36",
-    "cookie":
-    "yj0M_ae15_saltkey=n0ApaVFI; yj0M_ae15_lastvisit=1596979844; yj0M_ae15_ulastactivity=1597046078%7C0; yj0M_ae15_auth=659bo6fiKjfjYErypokCyV6mSSVv3USE123GmzmMVlD0a1sCkusWOJQc%2Bm0mjLS%2B0pWLQFdC4RAmFplTGu224ozMq9E; yj0M_ae15_lastcheckfeed=934165%7C1597046078; yj0M_ae15_lip=13.78.14.245%2C1597046078; yj0M_ae15_nofavfid=1; yj0M_ae15_noticeTitle=1; yj0M_ae15_sendmail=1; yj0M_ae15_lastact=1597046430%09misc.php%09patch",
+    "cookie":cookie
 }
 url1 = "https://bbs.kansoushu.com/home.php?mod=task&do=apply&id=2"
 res1 = requests.post(url=url1, headers=headers1).text
@@ -39,8 +39,7 @@ for i in range(100):
 headers3 = {
     "user-agent":
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36",
-    "cookie":
-    "yj0M_ae15_saltkey=n0ApaVFI; yj0M_ae15_lastvisit=1596979844; yj0M_ae15_ulastactivity=1597046078%7C0; yj0M_ae15_auth=659bo6fiKjfjYErypokCyV6mSSVv3USE123GmzmMVlD0a1sCkusWOJQc%2Bm0mjLS%2B0pWLQFdC4RAmFplTGu224ozMq9E; yj0M_ae15_lastcheckfeed=934165%7C1597046078; yj0M_ae15_lip=13.78.14.245%2C1597046078; yj0M_ae15_nofavfid=1; yj0M_ae15_noticeTitle=1; yj0M_ae15_sendmail=1; yj0M_ae15_lastact=1597046430%09misc.php%09patch",
+    "cookie":cookie
 }
 url3 = "https://bbs.kansoushu.com/home.php?mod=task&do=draw&id=2"
 res3 = requests.post(url=url3, headers=headers3).text
