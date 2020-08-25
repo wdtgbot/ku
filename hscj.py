81be4703fa4cccc7ed518098dcf38687f9654796
ziyonglixian <68988540+ziyonglixian@users.noreply.github.com>
2020-08-25 17:59:32 +0800

    Create hscj.py

---
+++ hscj.py
@@ -0,0 +1,23 @@
+import time
+
+import requests
+import os
+
+
+def q():
+    url = 'https://www.93book.com/plugin.php?id=it618_award:ajax&ac=getaward&formhash=359325ed&_=' + str(
+        int(time.time()))
+    url1 = 'https://www.93book.com/plugin.php?id=it618_award:ajax&formhash=359325ed&ac=getcredits'
+    cookie = os.environ["COOKIEHS"]
+    headers = {
+        'user-agent':
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36',
+        'cookie': cookie
+    }
+    res = requests.get(url=url, headers=headers).text
+    res1 = requests.get(url=url1, headers=headers).text
+    print(res1)
+
+
+for i in range(20):
+    q()
