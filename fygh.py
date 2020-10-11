cd7e1f405bdf238288ba4e9971d48bc8365940d7
ziyonglixian <68988540+ziyonglixian@users.noreply.github.com>
2020-10-11 23:57:59 +0800

    Update fygh.py

--- fygh.py
+++ fygh.py
@@ -6,7 +6,7 @@ import os
 
 cookie = os.environ["COOKIEFY"]
 def q():
-    url1 = 'https://bbs.fygal.com/kf_fw_ig_index.php'
+    url1 = 'https://bbs.kforz.com/kf_fw_ig_index.php'
     headers = {
         "user-agent":
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36",
@@ -15,7 +15,7 @@ def q():
     res1 = requests.post(url=url1, headers=headers).text
     a = re.findall(r'data: "safeid=(.+?)"', res1, re.S)
     a = a[-1]
-    url3 = 'https://bbs.fygal.com/kf_fw_ig_halo.php?do=buy&id=2&safeid=' + str(a)
+    url3 = 'https://bbs.kforz.com/kf_fw_ig_halo.php?do=buy&id=2&safeid=' + str(a)
     res3 = requests.get(url=url3, headers=headers).text
 
 
