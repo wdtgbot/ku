dba3556bcab601abda8dd676e973b6f91393766e
ziyonglixian <68988540+ziyonglixian@users.noreply.github.com>
2020-11-19 13:50:09 +0800

    Update hsrw.py

--- hsrw.py
+++ hsrw.py
@@ -6,7 +6,7 @@ import os
 cookie = os.environ["COOKIEHS"]
 headers1 = {
     "user-agent":
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36",
     "cookie":cookie
 }
 url1 = "https://www.93book.com/home.php?mod=task&do=apply&id=14"
