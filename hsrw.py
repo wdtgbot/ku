da057b45f09b790769bdf1f2a28479634bf72b34
ziyonglixian <68988540+ziyonglixian@users.noreply.github.com>
2020-12-02 14:01:54 +0800

    Update hsrw.py

--- hsrw.py
+++ hsrw.py
@@ -9,9 +9,9 @@ headers1 = {
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36",
     "cookie":cookie
 }
-url1 = "https://www.93book.com/home.php?mod=task&do=apply&id=14"
-url2 = "https://www.93book.com/home.php?mod=task&do=apply&id=15"
-url3 = "https://www.93book.com/home.php?mod=task&do=apply&id=16"
+url1 = "https://www.93hsy.com/home.php?mod=task&do=apply&id=14"
+url2 = "https://www.93hsy.com/home.php?mod=task&do=apply&id=15"
+url3 = "https://www.93hsy.com/home.php?mod=task&do=apply&id=16"
 res1 = requests.post(url=url1, headers=headers1).text
 res2 = requests.post(url=url2, headers=headers1).text
 res3 = requests.post(url=url3, headers=headers1).text
@@ -23,7 +23,7 @@ def q():
     x = random.randint(0, 255)
     y = random.randint(0, 255)
     ip = str(m) + "." + str(n) + "." + str(x) + "." + str(y)
-    url = 'https://www.93book.com/?fromuid=619687'
+    url = 'https://www.93hsy.com/?fromuid=619687'
     headers = {
         "user-agent":
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36",
@@ -46,9 +46,9 @@ headers3 = {
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36",
     "cookie":cookie
 }
-url4 = "https://www.93book.com/home.php?mod=task&do=draw&id=14"
-url5 = "https://www.93book.com/home.php?mod=task&do=draw&id=15"
-url6 = "https://www.93book.com/home.php?mod=task&do=draw&id=16"
+url4 = "https://www.93hsy.com/home.php?mod=task&do=draw&id=14"
+url5 = "https://www.93hsy.com/home.php?mod=task&do=draw&id=15"
+url6 = "https://www.93hsy.com/home.php?mod=task&do=draw&id=16"
 res4 = requests.post(url=url4, headers=headers3).text
 res5 = requests.post(url=url5, headers=headers3).text
 res6 = requests.post(url=url6, headers=headers3).text
