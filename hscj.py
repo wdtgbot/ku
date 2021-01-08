fb026c342176c42c9761c9d85a4148ea5684f4ca
ziyonglixian <68988540+ziyonglixian@users.noreply.github.com>
2021-01-08 12:06:32 +0800

    Update hscj.py

--- hscj.py
+++ hscj.py
@@ -5,13 +5,13 @@ import os
 
 formhash = os.environ["HASHHS"] 
 def q():
-    url = 'https://www.93hsy.com/plugin.php?id=it618_award:ajax&ac=getaward&formhash='+str(formhash)+'&_=' + str(
+    url = 'https://www.93hsy.com/plugin.php?id=it618_award:ajax&ac=getaward&formhash='+str(formhash)+ '&_=' + str(
     int(round(time.time() * 1000)))
     url1 = 'https://www.93hsy.com/plugin.php?id=it618_award:ajax&formhash='+str(formhash)+'&ac=getcredits'
     cookie = os.environ["COOKIEHS"]
     headers = {
         'user-agent':
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36',
         'cookie': cookie
     }
     res = requests.get(url=url, headers=headers).text
