151037b3f7ccb567fb952b2434fa61d2412d4d8f
ziyonglixian <68988540+ziyonglixian@users.noreply.github.com>
2021-01-18 10:21:59 +0800

    Create ss1.py

---
+++ 日常/ss1.py
@@ -0,0 +1,46 @@
+import random
+import threading
+import requests
+import os
+
+cookie = os.environ["COOKIESS1"]
+headers1 = {
+    "user-agent":
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36",
+    "cookie":cookie
+}
+url1 = "http://txt.soushupan.xyz:18866/home.php?mod=task&do=apply&id=2"
+res1 = requests.post(url=url1, headers=headers1).text
+
+
+def q():
+    m = random.randint(0, 255)
+    n = random.randint(0, 255)
+    x = random.randint(0, 255)
+    y = random.randint(0, 255)
+    ip = str(m) + "." + str(n) + "." + str(x) + "." + str(y)
+    url = 'http://txt.soushupan.xyz:18866/?fromuid=1076784'
+    headers = {
+        "user-agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36",
+        "X-forwarded-for":
+        str(ip),
+        "Client_ip":
+        str(ip),
+        "cookie":
+        "yj0M_5ed6_saltkey=rx9ucDZ0; yj0M_5ed6_lastvisit=1596710921; yj0M_5ed6_promotion=941145; yj0M_5ed6_lastact=1596714523%09home.php%09misc; yj0M_5ed6_sendmail=1"
+    }
+    requests.get(url=url, headers=headers).text
+
+
+for i in range(100):
+    t = threading.Thread(target=q)
+    t.start()
+
+headers3 = {
+    "user-agent":
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36",
+    "cookie":cookie
+}
+url3 = "http://txt.soushupan.xyz:18866/home.php?mod=task&do=draw&id=2"
+res3 = requests.post(url=url3, headers=headers3).text
