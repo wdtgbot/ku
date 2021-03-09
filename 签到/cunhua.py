655f23ba85d68f9ceee10af636f02c481663fca7
ziyonglixian <68988540+ziyonglixian@users.noreply.github.com>
2021-03-09 16:50:25 +0800

    Update cunhua.py

--- 签到/cunhua.py
+++ 签到/cunhua.py
@@ -5,10 +5,10 @@ import os
 cookie =  os.environ["COOKIECH"]
 formhash =  os.environ["FORMHASHCH"]
 def q():
-    url = 'https://www.cunhua.guru/k_misign-sign.html?operation=qiandao&format=global_usernav_extra&formhash='+str(formhash)+'&inajax=1&ajaxtarget=k_misign_topb'
+    url = 'https://www.cunhua.casa/k_misign-sign.html?operation=qiandao&format=global_usernav_extra&formhash='+str(formhash)+'&inajax=1&ajaxtarget=k_misign_topb'
     headers = {
         "user-agent":
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36",
         "cookie": cookie,
     }
     res = requests.post(url=url, headers=headers).text
